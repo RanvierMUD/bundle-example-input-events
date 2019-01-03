25305432fec0ee7b9e6b3aa081ee10bb500de410
Shawn Biddle <shawncplus@gmail.com>
2019-01-02 21:44:41 -0500

    Get rid of RoomManager.startingRoom

--- input-events/finish-player.js
+++ input-events/finish-player.js
@@ -1,47 +1,54 @@
 'use strict';
 
-const { Player } = require('ranvier');
+const { Config, Player } = require('ranvier');
 
 /**
  * Finish player creation. Add the character to the account then add the player
  * to the game world
  */
 module.exports = {
-  event: state => async (socket, args) => {
-    let player = new Player({
-      name: args.name,
-      account: args.account,
-    });
-
-
-    // TIP:DefaultAttributes: This is where you can change the default attributes for players
-    const defaultAttributes = {
-      health: 100,
-      strength: 20,
-      agility: 20,
-      intellect: 20,
-      stamina: 20,
-      armor: 0,
-      critical: 0
-    };
-
-    for (const attr in defaultAttributes) {
-      player.addAttribute(state.AttributeFactory.create(attr, defaultAttributes[attr]));
+  event: state => {
+    const startingRoomRef = Config.get('startingRoom');
+    if (!startingRoomRef) {
+      Logger.error('No startingRoom defined in ranvier.json');
     }
 
-    args.account.addCharacter(args.name);
-    args.account.save();
+    return async (socket, args) => {
+      let player = new Player({
+        name: args.name,
+        account: args.account,
+      });
 
-    player.setMeta('class', args.playerClass);
 
-    const room = state.RoomManager.startingRoom;
-    player.room = room;
-    player.save();
+      // TIP:DefaultAttributes: This is where you can change the default attributes for players
+      const defaultAttributes = {
+        health: 100,
+        strength: 20,
+        agility: 20,
+        intellect: 20,
+        stamina: 20,
+        armor: 0,
+        critical: 0
+      };
 
-    // reload from manager so events are set
-    player = await state.PlayerManager.loadPlayer(state, player.account, player.name);
-    player.socket = socket;
+      for (const attr in defaultAttributes) {
+        player.addAttribute(state.AttributeFactory.create(attr, defaultAttributes[attr]));
+      }
 
-    socket.emit('done', socket, { player });
+      args.account.addCharacter(args.name);
+      args.account.save();
+
+      player.setMeta('class', args.playerClass);
+
+      const room = state.RoomManager.get(startingRoomRef);
+      player.room = room;
+      player.save();
+
+      // reload from manager so events are set
+      player = await state.PlayerManager.loadPlayer(state, player.account, player.name);
+      player.socket = socket;
+
+      socket.emit('done', socket, { player });
+    };
   }
 };
